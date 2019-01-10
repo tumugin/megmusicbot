fed50baca7ca579c90bcd01ce73f51529085a319
Kazuki Oishi <oishikazuki@gmail.com>
2019-01-11 00:11:36 +0900

    [WIP] キュー周りの実装

--- src/main/kotlin/com/myskng/megmusicbot/bot/SongQueueManager.kt
+++ src/main/kotlin/com/myskng/megmusicbot/bot/SongQueueManager.kt
@@ -1,4 +1,21 @@
 package com.myskng.megmusicbot.bot
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.withContext
+import sx.blah.discord.handle.audio.IAudioManager
+
 class SongQueueManager {
+    val songQueue = mutableListOf<ISong>()
+    var playingSong: ISong? = null
+    suspend fun playQueue(audioManager: IAudioManager) {
+        withContext(Dispatchers.Default) {
+            while (isActive) {
+                if (songQueue.isNotEmpty()) {
+                    playingSong = songQueue.removeAt(0)
+                    playingSong?.play(audioManager)
+                }
+            }
+        }
+    }
 }
